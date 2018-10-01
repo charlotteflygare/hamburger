416f6c98a0bbe6fca7fa3f42668623f00e133393
charlotteflygare <38113960+charlotteflygare@users.noreply.github.com>
2018-10-01 19:17:41 +0200

    Menu in place

--- code/src/components/header/index.js
+++ code/src/components/header/index.js
@@ -8,12 +8,22 @@ class Header extends React.Component {
   render() {
     return (
       <header>
+      <div className="container">
+        <label for="hamburger" class="hamburger-label" >
+          <div class="menu">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+          </div>
+        </label>
+        <input id="hamburger" class="hamburger-checkbox" type="checkbox"  />
         <Grid className="header-grid">
           <a href="http://technigo.io">Technigo</a>
           <a href="http://technigo.io">Boot Camp</a>
           <a href="http://technigo.io">Stories</a>
           <a href="http://technigo.io">About</a>
         </Grid>
+        </div>
       </header>
     )
   }
